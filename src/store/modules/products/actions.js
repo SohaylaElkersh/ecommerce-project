7ff78705781dc0f369416d398000365fb58aa08e
Sohayla EL-Kersh <lalyelkersh@gmail.com>
2026-04-15 18:14:07 +0200

    refactor: replace fetch calls with API service functions in product actions

--- src/store/modules/products/actions.js
+++ src/store/modules/products/actions.js
@@ -1,47 +1,45 @@
+import {
+  fetchProductsApi,
+  fetchProductApi,
+  fetchRelatedProductsApi,
+  fetchCategoriesApi
+} from "@/services/api/products";
+
 export default {
-  fetchProducts({ commit, state }) {
-    return fetch(`https://dummyjson.com/products?limit=${state.limit}&skip=${state.skip}&select=id,title,thumbnail,price,rating,reviews,discountPercentage`)
-      .then(res => res.json())
-      .then(data => {
-        commit('appendProducts', data.products)
-        commit('incrementSkip')
-        commit('setTotal', data.total)
-      })
+  async fetchProducts({ commit, state }) {
+    const data = await fetchProductsApi({
+      limit: state.limit,
+      skip: state.skip
+    });
+
+    commit('appendProducts', data.products);
+    commit('incrementSkip');
+    commit('setTotal', data.total);
   },
 
-  fetchProduct({ commit }, id) {
-    return fetch(`https://dummyjson.com/products/${id}?select=id,thumbnail,images,title,price,rating,description,reviews,discountPercentage,category,stock,description`)
-      .then(res => {
-        if (!res.ok) throw new Error("Product not found")
-        return res.json()
-      })
-      .then(data => {
-        commit('setProduct', data)
-        return data
-      })
-      .catch(error => {
-        commit('setProduct', null)
-        throw error
-      })
+  async fetchProduct({ commit }, id) {
+    try {
+      const data = await fetchProductApi(id);
+      commit('setProduct', data);
+      return data;
+    } catch (error) {
+      commit('setProduct', null);
+      throw error;
+    }
   },
 
-  fetchRelatedProducts({ commit }, { category, productId }) {
-    return fetch(`https://dummyjson.com/products/category/${category}`)
-      .then(res => res.json())
-      .then(data => {
-        const filtered = data.products
-          .filter(p => p.id !== productId)
-          .slice(0, 4)
+  async fetchRelatedProducts({ commit }, { category, productId }) {
+    const data = await fetchRelatedProductsApi(category);
 
-        commit('setRelatedProducts', filtered)
-      })
+    const filtered = data.products
+      .filter(p => p.id !== productId)
+      .slice(0, 4);
+
+    commit('setRelatedProducts', filtered);
   },
 
-  fetchCategories({ commit }) {
-    return fetch('https://dummyjson.com/products/categories')
-      .then(res => res.json())
-      .then(data => {
-        commit('setCategories', data.slice(0, 8))
-      })
+  async fetchCategories({ commit }) {
+    const data = await fetchCategoriesApi();
+    commit('setCategories', data.slice(0, 8));
   }
-}
+};
