dcc35ab9f0aa5a7fa29fd2ab2016f60d39893a1c
Sohayla EL-Kersh <lalyelkersh@gmail.com>
2026-04-19 10:11:14 +0200

    trying to fix vercel bug

--- src/services/api/products.js
+++ src/services/api/products.js
@@ -2,7 +2,7 @@ import { api } from "./axios";
 
 
 export const fetchProductsApi = async ({ limit, skip }) => {
-  const res = await api.get("/products", {
+  const res = await api.get("", {
     params: { limit, skip, select: "id,title,thumbnail,price,rating,reviews,discountPercentage", },
   });
   return res.data;
@@ -10,7 +10,7 @@ export const fetchProductsApi = async ({ limit, skip }) => {
 
 export const fetchProductApi = async (id) => {
   try {
-    const res = await api.get(`/products/${id}`, {
+    const res = await api.get(`/${id}`, {
       params: { select: "id,thumbnail,images,title,price,rating,description,reviews,discountPercentage,category,stock,description", },
     });
     return res.data;
@@ -25,12 +25,12 @@ export const fetchRelatedProductsApi = async (category) => {
 };
 
 export const fetchCategoriesApi = async () => {
-  const res = await api.get("/products/categories");
+  const res = await api.get("/categories");
   return res.data;
 };
 
 export const fetchProductsByCategoryApi = async ({ category, limit, skip, }) => {
-  const res = await api.get(`/products/category/${category}`, {
+  const res = await api.get(`/category/${category}`, {
     params: { limit, skip, select: "id,title,thumbnail,price,rating,reviews,discountPercentage", },
   });
   return res.data;
